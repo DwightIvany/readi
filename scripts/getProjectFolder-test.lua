ea6b2c3cc146891b8abd6307222d33771a4ef9b4
DwightIvany <dwight.ivany@gmail.com>
2024-09-28 09:27:07 -0700

    Update getProjectFolder-test.lua

--- scripts/getProjectFolder-test.lua
+++ scripts/getProjectFolder-test.lua
@@ -1,18 +1,26 @@
+--[[
+I am quite organized in how I structure my Project folders.
+If my scripts knew the path of the reaper project without the filename,
+they could easily run without my user input.
+
+Oddly it does not seem to be built in https://forums.cockos.com/showthread.php?t=189922
+It seems odd to me but this function will do this, so I have saved it in my repo.
+
+I still need to test this on a Mac 2024-09-28    
+]]--
+
 -- Function to get the folder path of the current project
 function GetProjectFolder()
     -- Get the full project path including the .rpp file
     local proj, projectPath = reaper.EnumProjects(-1, "")
-    
-    -- Find the last occurrence of the directory separator (backslash or forward slash)
-    -- This handles both Windows and Linux/Mac path formats
-    local sep = string.match(projectPath, "[/\\]")
-    
+    -- Find the last slash of GetProjectFolder both Windows and Linux/Mac
+    local lastSlash = string.match(projectPath, "[/\\]")
+    reaper.ShowConsoleMsg("\nlastSlash: " .. lastSlash)
     -- Strip the file name to get only the directory path
-    local folderPath = string.match(projectPath, "(.*" .. sep .. ")")
-    
+    local folderPath = string.match(projectPath, "(.*" .. lastSlash .. ")")   
     return folderPath
 end
 
 -- Testing the function
 local projectFolder = GetProjectFolder()
-reaper.ShowConsoleMsg("Project folder: " .. projectFolder)
+reaper.ShowConsoleMsg("\nProject folder: " .. projectFolder)
