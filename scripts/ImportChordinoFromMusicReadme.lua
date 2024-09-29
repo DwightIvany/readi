f1dbe1430814d3f52135586918c06fa9ed331ca1
DwightIvany <dwight.ivany@gmail.com>
2024-09-28 19:40:10 -0700

    Update ImportChordinoFromMusicReadme.lua early version with marker export

--- scripts/ImportChordinoFromMusicReadme.lua
+++ scripts/ImportChordinoFromMusicReadme.lua
@@ -239,6 +239,49 @@ function main()
   reaper.ShowConsoleMsg("csvChordinoInput: " .. csvChordinoInput .. "\n")
 --End my Hack
 
+--
+-- Export existing markers
+-- Define the output file path
+local exportMarkerPath = "G:\\Data\\Dropbox\\ToDo\\music-readme\\chordino\\" .. projectFileNameNoExt .. " -markers.csv"
+
+-- Open the output file for writing
+local file = io.open(exportMarkerPath, "w")
+if not file then
+  reaper.ShowMessageBox("Error opening file for writing", "Error", 0)
+  return
+end
+
+-- Write the header row
+file:write("Index,Type,Position,Length,Name\n")
+
+-- Get the number of markers and regions in the project
+local retval, numMarkers, numRegions = reaper.CountProjectMarkers(0)
+
+-- Loop through all markers and regions
+for i = 0, numMarkers + numRegions - 1 do
+  -- Get details for the current marker or region
+  local retval, isRegion, position, regionEnd, name, idx = reaper.EnumProjectMarkers(i)
+
+  -- Determine if it's a marker or a region
+  local markerType = isRegion and "Region" or "Marker"
+
+  -- Calculate the length of the region (for markers, this will be 0)
+  local length = isRegion and (regionEnd - position) or 0
+
+  -- Write the marker/region data to the CSV file
+  file:write(string.format("%d,%s,%.4f,%.4f,%s\n", idx, markerType, position, length, name))
+end
+
+-- Close the file
+file:close()
+
+reaper.ShowMessageBox("Markers and regions exported to C:\\markers.csv", "Export complete", 0)
+
+
+-- ToDo delete the old version or overwrite
+-- End of marker export
+
+
   folder = csvChordinoInput:match[[^@?(.*[\/])[^\/]-$]]
 
   for i, line in ipairs( lines ) do
@@ -268,7 +311,7 @@ end
 
 -- ToDo in debug, I needed my hack here maybe a local problem
 local folderPath, projectFileName, projectFileNameNoExt = GetProjectPaths()
-local csvChordinoInput = "G:\\Data\\Dropbox\\ToDo\\music-readme\\chordino\\" .. projectFileNameNoExt .. "-chordino.csv"
+local csvChordinoInput = "G:\\Data\\Dropbox\\ToDo\\music-readme\\chordino\\" .. projectFileNameNoExt .. " -chordino.csv" --todo get this down to once
 reaper.ShowConsoleMsg("csvChordinoInput: " .. csvChordinoInput .. "\n")
 
 read_lines(csvChordinoInput)
